6de024fe8dd8dd66864b220c4387a323a83bdcb5
walidaOmar <walidawalid0963@gmail.com>
2025-12-11 00:40:06 +0000

    backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Connect DB
+connectDB();
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
